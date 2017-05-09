d86b40182489a938ca14f4627af31990e24e963d
Ross Murray <Ross Murray>
2017-05-09 15:43:19 +1000

    enable32bit changed to if else that results in bool

--- lib/puppet/type/iis_pool.rb
+++ lib/puppet/type/iis_pool.rb
@@ -27,9 +27,9 @@ Puppet::Type.newtype(:iis_pool) do
     desc 'A Boolean to determine if 32bit mode should enabled.'
     newvalues(:false, :true, :False, :True, false, true)
     munge do |value|
-      if value.to_s == 'true' || :True || :true
+      if ["true", :True, :true].include? value.to_s
         true
-      elsif value.to_s == 'false' || :False || :false
+      else
         false
       end
     end
