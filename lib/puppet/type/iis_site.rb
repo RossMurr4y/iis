759c01af86bc64f79fddf36854a723b600020024
Ross Murray <Ross Murray>
2017-05-16 16:54:05 +1000

    ran rubocop and found issue with missing DO

--- lib/puppet/type/iis_site.rb
+++ lib/puppet/type/iis_site.rb
@@ -8,32 +8,30 @@ Puppet::Type.newtype(:iis_site) do
   # Setting this title_pattern will ensure a resource title sets the :name to the same value.
   # Without this, it will try to assign it to the key-attribute. Both :name and :path (which
   # is the namevar) are key-attributes so Puppet will fall over.
-  def self.title_patterns  
-       [ [ /(.*)/m, [ [:name] ] ] ] 
-  end 
+  def self.title_patterns
+    [[/(.*)/m, [[:name]]]]
+  end
 
   ### parameters
-  newparam(:name, :namevar => true) do
+  newparam(:name, namevar: true) do
     desc 'The displayname for the website.'
     validate do |value|
-      fail("#{name} is not a valid website name") unless value =~ %r{^[a-zA-Z0-9\/\-\_\.'\s]+$}
+      raise("#{name} is not a valid website name") unless value =~ %r{^[a-zA-Z0-9\/\-\_\.'\s]+$}
     end
     defaultto :path
   end
 
-  ### properties 
+  ### properties
   newproperty(:path) do
     desc 'The fully-qualified filepath to the root of the IIS Site. '
     validate do |value|
-      fail("Invalid path value of #{value}") unless Pathname.new(value).absolute?
+      raise("Invalid path value of #{value}") unless Pathname.new(value).absolute?
     end
-  end 
+  end
 
   newproperty(:state) do
     desc 'The state to enforce upon the Site.'
-    munge do |value|
-      value.capitalize
-    end
+    munge(&:capitalize)
     newvalues(:stopped, :Stopped, :started, :Started)
     defaultto :Started
   end
@@ -41,15 +39,15 @@ Puppet::Type.newtype(:iis_site) do
   newproperty(:app_pool) do
     desc 'The Application Pool that the IIS site should use.'
     validate do |value|
-      fail("#{app_pool} is not a valid Application Pool name") unless value =~ %r{[a-zA-Z0-9\-\_\'\s]+$}
+      raise("#{app_pool} is not a valid Application Pool name") unless value =~ /[a-zA-Z0-9\-\_\'\s]+$/
     end
     defaultto :DefaultAppPool
   end
 
   newproperty(:hostheader) do
-    desc "The Websites host header."
+    desc 'The Websites host header.'
     validate do |value|
-      fail("#{hostheader} is not a valid Host Header for a Site.") unless value =~ %r{[a-zA-Z0-9\-\_\'\.\s]+$} || value == :false
+      raise("#{hostheader} is not a valid Host Header for a Site.") unless value =~ /[a-zA-Z0-9\-\_\'\.\s]+$/ || value == :false
     end
   end
 
@@ -62,8 +60,8 @@ Puppet::Type.newtype(:iis_site) do
   newproperty(:ip) do
     desc 'The IP Address the site should use. Valid for both IPv4 and IPv6 addresses.'
     validate do |value|
-      unless ((value =~ Resolv::IPv4::Regex) || (value =~ Resolv::IPv6::Regex) || value == :"*") ? true : false
-        fail("Invalid IP value. #{value} is not a valid IPv4/6 Address format.")
+      unless (value =~ Resolv::IPv4::Regex) || (value =~ Resolv::IPv6::Regex) || value == :"*" ? true : false
+        raise("Invalid IP value. #{value} is not a valid IPv4/6 Address format.")
       end
     end
     defaultto :"*"
@@ -81,22 +79,20 @@ Puppet::Type.newtype(:iis_site) do
     defaultto :false
   end
 
-  newproperty(:authtypes, :array_matching => :all)
-    desc 'An array of all enabled Authentication Types (Anon, Basic, Digest, Windows). Absent values are disabled.'
-    newvalues(:Anonymous, :ASP, :Basic, :Digest, :Forms, :Windows)
-    aliasvalue(:A, :Anonymous)
-    aliasvalue(:Impersonation, :ASP)
-    aliasvalue(:B, :Basic)
-    aliasvalue(:D, :Digest)
-    aliasvalue(:F, :Forms)
-    aliasvalue(:W, :Windows)
-    munge do |value|
-      value.capitalize
-    end
+  newproperty(:authtypes, array_matching: :all) do
+  desc 'An array of all enabled Authentication Types (Anon, Basic, Digest, Windows). Absent values are disabled.'
+  newvalues(:Anonymous, :ASP, :Basic, :Digest, :Forms, :Windows)
+  aliasvalue(:A, :Anonymous)
+  aliasvalue(:Impersonation, :ASP)
+  aliasvalue(:B, :Basic)
+  aliasvalue(:D, :Digest)
+  aliasvalue(:F, :Forms)
+  aliasvalue(:W, :Windows)
+  munge(&:capitalize)
   end
 
   autorequire(:iis_pool) do
     self[:app_pool] if @parameters.include? :app_pool
   end
-  
+
 end
